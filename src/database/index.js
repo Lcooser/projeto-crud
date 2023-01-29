8ca4275e4a8f51d26d4ad4f623354062e8ae6649
Luiz Coser <luiz.cooser1@gmail.com>
2023-01-29 13:27:19 -0300

    adicionando Document Schema(model)

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connect(){
     mongoose.set('strictQuery', false);
-    mongoose.connect('mongodb://127.0.0.1:27017')
+    mongoose.connect('mongodb://127.0.0.1:27017/projeto-crud')
 
 
     const db = mongoose.connection
