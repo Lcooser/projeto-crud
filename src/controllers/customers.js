6e223fe76dc62921f1685cccd489b6eada253471
Luiz Coser <luiz.cooser1@gmail.com>
2023-01-30 12:29:47 -0300

    Adicionando bcrypt, criptografando senhas do usuário

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,20 +1,23 @@
 const CustomersModel = require('../models/customers')
+const { crypto } = require('../utils/password')
 
 
-
-function  add(req, res){
+async function  add(req, res){
     const {
         name,
         age,
         email,
-        password
+        password,
     } = req.body
+
+    const passwordCrypto = await crypto(password)
+
 
     const register = new CustomersModel({
         name, 
         age,
         email,
-        password,
+        password: passwordCrypto,
     })
 
     register.save()
